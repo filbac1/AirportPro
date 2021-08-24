22892946d23dcb48db73d5db1f4504ad77653942
Filip <filipbacic08@gmail.com>
2021-08-24 10:56:20 +0200

    Dodan package facade

--- src/main/java/com/airport/airportproject/service/impl/AirportServiceImpl.java
+++ src/main/java/com/airport/airportproject/service/impl/AirportServiceImpl.java
@@ -1,6 +1,7 @@
 package com.airport.airportproject.service.impl;
 
 import com.airport.airportproject.exception.NoFlightsException;
+import com.airport.airportproject.facade.AirportFacade;
 import com.airport.airportproject.form.AirportTableForm;
 import com.airport.airportproject.service.AirportService;
 import com.amadeus.Amadeus;
@@ -20,9 +21,11 @@ import java.util.Map;
 public class AirportServiceImpl implements AirportService {
 
     private final Amadeus amadeus;
+    private final AirportFacade airportFacade;
     private Map<AirportTableForm, List<AirportTableForm>> map = new HashMap<>();
 
-    public AirportServiceImpl() {
+    public AirportServiceImpl(AirportFacade airportFacade) {
+        this.airportFacade = airportFacade;
         this.amadeus = Amadeus
                 .builder("xaHk1zkqebjwYpWnmpQvsOrMWeABjUcG", "Ta3qLdtXKkt23iSk")
                 .build();
@@ -36,31 +39,7 @@ public class AirportServiceImpl implements AirportService {
                 System.out.println(map.get(searchForm).get(i) + "Here we go again");
             }
         } else {
-            List<AirportTableForm> list = new ArrayList<>();
-            FlightOfferSearch[] flightOffersSearches = amadeus.shopping.flightOffersSearch.get(
-                    Params.with("originLocationCode", IATAfrom)
-                            .and("destinationLocationCode", IATAto)
-                            .and("departureDate", dateFrom)
-                            .and("returnDate", dateTo)
-                            .and("adults", passengerNum)
-                            .and("max", 200));
-                if (flightOffersSearches.length != 0) {
-                    for (int i = 0; i < flightOffersSearches.length; i++) {  // broj ponuda
-                        if (currency.equals(flightOffersSearches[i].getPrice().getCurrency())) {
-                            AirportTableForm tableForm = new AirportTableForm(IATAfrom, IATAto, dateFrom, dateTo,
-                                    flightOffersSearches[i].getItineraries()[0].getSegments().length - 1,
-                                    flightOffersSearches[i].getItineraries()[1].getSegments().length - 1,
-                                    passengerNum, flightOffersSearches[i].getPrice().getCurrency(),
-                                    flightOffersSearches[i].getPrice().getGrandTotal());
-                            System.out.println(tableForm);
-                            list.add(tableForm);
-                        }
-                    }
-                    map.put(searchForm, list);
-            } else {
-                throw new NoFlightsException("There are no flights on selected dates or " +
-                        "there are no bookable seats for the selected number of passengers");
-            }
+            airportFacade.get(amadeus, map, IATAfrom, IATAto, dateFrom, dateTo, passengerNum, currency, searchForm);
         }
     }
 }
